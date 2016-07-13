fb5b4021e8ca87d837548f8e03f6d0c35139e749
Maxime Tricoire <max.tricoire@gmail.com>
2016-07-13 11:44:50 +0200

    v2.2.1: allow KevoreeLogger.setLogLevel() to be called directly with a string level

--- browser/kevoree-commons.js
+++ browser/kevoree-commons.js
@@ -101,7 +101,7 @@ var Bootstrapper = Class({
 
 module.exports = Bootstrapper;
 
-},{"pseudoclass":10}],3:[function(require,module,exports){
+},{"pseudoclass":9}],3:[function(require,module,exports){
 var Class = require('pseudoclass');
 
 var FileSystem = Class({
@@ -137,121 +137,126 @@ var getBrowserFileSystem = function getBrowserFileSystem(fsapi, size, callback) 
 };
 
 module.exports = FileSystem;
-},{"pseudoclass":10}],4:[function(require,module,exports){
-var Class  = require('pseudoclass'),
-    chalk  = require('chalk');
+},{"pseudoclass":9}],4:[function(require,module,exports){
+var Class = require('pseudoclass'),
+  chalk = require('chalk');
 
 var LEVELS = ['all', 'debug', 'info', 'warn', 'error', 'quiet'];
 
-var chalkInfo       = chalk.grey,
-    chalkWarn       = chalk.grey.bgYellow,
-    chalkWarnMsg    = chalk.yellow,
-    chalkError      = chalk.white.bgRed,
-    chalkErrorMsg   = chalk.red,
-    chalkDebug      = chalk.cyan;
+var chalkInfo = chalk.grey,
+  chalkWarn = chalk.grey.bgYellow,
+  chalkWarnMsg = chalk.yellow,
+  chalkError = chalk.white.bgRed,
+  chalkErrorMsg = chalk.red,
+  chalkDebug = chalk.cyan;
 
 var KevoreeLogger = Class({
-    toString: 'KevoreeLogger',
+  toString: 'KevoreeLogger',
 
-    construct: function (tag) {
-        this.tag = tag;
-        this.level = 2;
-        this.filter = '';
-    },
+  construct: function (tag) {
+    this.tag = tag;
+    this.level = 2;
+    this.filter = '';
+  },
 
-    info: function (tag, msg) {
-        if (this.level <= LEVELS.indexOf('info')) {
-            if (typeof(msg) === 'undefined') {
-                msg = tag;
-                tag = this.tag;
-            }
+  info: function (tag, msg) {
+    if (this.level <= LEVELS.indexOf('info')) {
+      if (typeof (msg) === 'undefined') {
+        msg = tag;
+        tag = this.tag;
+      }
 
-            if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
-                console.log(getTime()+'  '+chalkInfo('INFO')+'   '+processTag(tag)+'  '+chalkInfo(msg));
-            }
-        }
-    },
-
-    debug: function (tag, msg) {
-        if (this.level <= LEVELS.indexOf('debug')) {
-            if (typeof(msg) === 'undefined') {
-                msg = tag;
-                tag = this.tag;
-            }
-
-            if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
-                console.log(getTime()+'  '+chalkDebug('DEBUG ')+' '+processTag(tag)+'  '+chalkDebug(msg));
-            }
-        }
-    },
-
-    warn: function (tag, msg) {
-        if (this.level <= LEVELS.indexOf('warn')) {
-            if (typeof(msg) === 'undefined') {
-                msg = tag;
-                tag = this.tag;
-            }
-
-            if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
-                console.warn(getTime()+'  '+chalkWarn('WARN')+'   '+processTag(tag)+'  '+chalkWarnMsg(msg));
-            }
-        }
-    },
-
-    error: function (tag, msg) {
-        if (this.level <= LEVELS.indexOf('error')) {
-            if (typeof(msg) === 'undefined') {
-                msg = tag;
-                tag = this.tag;
-            }
-
-            if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
-                console.error(getTime() + '  ' + chalkError('ERROR') + '  ' + processTag(tag) + '  ' + chalkErrorMsg(msg));
-            }
-        }
-    },
-
-    setLevel: function (level) {
-        this.level = level;
-        console.log(getTime()+'  '+chalkInfo('ALL ')+'   '+processTag(this.toString())+'  '+chalkInfo('Set logLevel= '+LEVELS[this.level]));
-    },
-
-    setFilter: function (filter) {
-        this.filter = filter;
-        console.log(getTime()+'  '+chalkInfo('ALL ')+'   '+processTag(this.toString())+'  '+chalkInfo('Set logFilter= "'+this.filter+'"'));
+      if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
+        console.log(getTime() + '  ' + chalkInfo('INFO') + '   ' + processTag(tag) + '  ' + chalkInfo(msg));
+      }
     }
+  },
+
+  debug: function (tag, msg) {
+    if (this.level <= LEVELS.indexOf('debug')) {
+      if (typeof (msg) === 'undefined') {
+        msg = tag;
+        tag = this.tag;
+      }
+
+      if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
+        console.log(getTime() + '  ' + chalkDebug('DEBUG ') + ' ' + processTag(tag) + '  ' + chalkDebug(msg));
+      }
+    }
+  },
+
+  warn: function (tag, msg) {
+    if (this.level <= LEVELS.indexOf('warn')) {
+      if (typeof (msg) === 'undefined') {
+        msg = tag;
+        tag = this.tag;
+      }
+
+      if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
+        console.warn(getTime() + '  ' + chalkWarn('WARN') + '   ' + processTag(tag) + '  ' + chalkWarnMsg(msg));
+      }
+    }
+  },
+
+  error: function (tag, msg) {
+    if (this.level <= LEVELS.indexOf('error')) {
+      if (typeof (msg) === 'undefined') {
+        msg = tag;
+        tag = this.tag;
+      }
+
+      if (this.filter.length === 0 || (this.filter.length > 0 && tag === this.filter)) {
+        console.error(getTime() + '  ' + chalkError('ERROR') + '  ' + processTag(tag) + '  ' + chalkErrorMsg(msg));
+      }
+    }
+  },
+
+  setLevel: function (level) {
+    if (typeof level === 'string') {
+      this.level = LEVELS.indexOf(level.trim().toLowerCase());
+    } else {
+      this.level = level;
+    }
+    console.log(getTime() + '  ' + chalkInfo('ALL ') + '   ' + processTag(this.toString()) + '  ' + chalkInfo('Set logLevel= ' + LEVELS[this.level]));
+  },
+
+  setFilter: function (filter) {
+    this.filter = filter;
+    console.log(getTime() + '  ' + chalkInfo('ALL ') + '   ' + processTag(this.toString()) + '  ' + chalkInfo('Set logFilter= "' + this.filter + '"'));
+  }
 });
 
 var processTag = function processTag(tag) {
-    if (tag.length >= 15) {
-        tag = tag.substr(0, 14)+'.';
-    } else {
-        var spaces = '';
-        for (var i=0; i < 15 - tag.length; i++) spaces += ' ';
-        tag += spaces;
-    }
+  if (tag.length >= 15) {
+    tag = tag.substr(0, 14) + '.';
+  } else {
+    var spaces = '';
+    for (var i = 0; i < 15 - tag.length; i++) spaces += ' ';
+    tag += spaces;
+  }
 
-    return chalk.magenta(tag);
+  return chalk.magenta(tag);
 };
 
 var getTime = function getTime() {
-    var time = new Date();
-    var hours = (time.getHours().toString().length == 1) ? '0'+time.getHours() : time.getHours();
-    var mins = (time.getMinutes().toString().length == 1) ? '0'+time.getMinutes() : time.getMinutes();
-    var secs = (time.getSeconds().toString().length == 1) ? '0'+time.getSeconds() : time.getSeconds();
-    return chalk.grey(hours+':'+mins+':'+secs);
+  var time = new Date();
+  var hours = (time.getHours().toString().length == 1) ? '0' + time.getHours() : time.getHours();
+  var mins = (time.getMinutes().toString().length == 1) ? '0' + time.getMinutes() : time.getMinutes();
+  var secs = (time.getSeconds().toString().length == 1) ? '0' + time.getSeconds() : time.getSeconds();
+  return chalk.grey(hours + ':' + mins + ':' + secs);
 };
 
 
-KevoreeLogger.ALL   = LEVELS.indexOf('all');
-KevoreeLogger.INFO  = LEVELS.indexOf('info');
+KevoreeLogger.ALL = LEVELS.indexOf('all');
+KevoreeLogger.INFO = LEVELS.indexOf('info');
 KevoreeLogger.DEBUG = LEVELS.indexOf('debug');
-KevoreeLogger.WARN  = LEVELS.indexOf('warn');
+KevoreeLogger.WARN = LEVELS.indexOf('warn');
 KevoreeLogger.ERROR = LEVELS.indexOf('error');
 KevoreeLogger.QUIET = LEVELS.indexOf('quiet');
 
 module.exports = KevoreeLogger;
-},{"chalk":7,"pseudoclass":10}],5:[function(require,module,exports){
+
+},{"chalk":6,"pseudoclass":9}],5:[function(require,module,exports){
 var Class = require('pseudoclass'),
     KevoreeLogger = require('./KevoreeLogger');
 
@@ -287,47 +292,7 @@ var Resolver = Class({
 
 module.exports = Resolver;
 
-},{"./KevoreeLogger":4,"pseudoclass":10}],6:[function(require,module,exports){
-'use strict';
-var styles = module.exports;
-
-var codes = {
-	reset: [0, 0],
-
-	bold: [1, 22],
-	italic: [3, 23],
-	underline: [4, 24],
-	inverse: [7, 27],
-	strikethrough: [9, 29],
-
-	black: [30, 39],
-	red: [31, 39],
-	green: [32, 39],
-	yellow: [33, 39],
-	blue: [34, 39],
-	magenta: [35, 39],
-	cyan: [36, 39],
-	white: [37, 39],
-	gray: [90, 39],
-
-	bgBlack: [40, 49],
-	bgRed: [41, 49],
-	bgGreen: [42, 49],
-	bgYellow: [43, 49],
-	bgBlue: [44, 49],
-	bgMagenta: [45, 49],
-	bgCyan: [46, 49],
-	bgWhite: [47, 49]
-};
-
-Object.keys(codes).forEach(function (key) {
-	var val = codes[key];
-	var style = styles[key] = {};
-	style.open = '\x1b[' + val[0] + 'm';
-	style.close = '\x1b[' + val[1] + 'm';
-});
-
-},{}],7:[function(require,module,exports){
+},{"./KevoreeLogger":4,"pseudoclass":9}],6:[function(require,module,exports){
 'use strict';
 var ansi = require('ansi-styles');
 var stripAnsi = require('strip-ansi');
@@ -392,7 +357,7 @@ if (chalk.enabled === undefined) {
 	chalk.enabled = chalk.supportsColor;
 }
 
-},{"ansi-styles":6,"has-color":8,"strip-ansi":11}],8:[function(require,module,exports){
+},{"ansi-styles":8,"has-color":7,"strip-ansi":11}],7:[function(require,module,exports){
 (function (process){
 'use strict';
 module.exports = (function () {
@@ -428,100 +393,47 @@ module.exports = (function () {
 })();
 
 }).call(this,require('_process'))
-},{"_process":9}],9:[function(require,module,exports){
-// shim for using process in browser
+},{"_process":10}],8:[function(require,module,exports){
+'use strict';
+var styles = module.exports;
 
-var process = module.exports = {};
-var queue = [];
-var draining = false;
-var currentQueue;
-var queueIndex = -1;
+var codes = {
+	reset: [0, 0],
 
-function cleanUpNextTick() {
-    draining = false;
-    if (currentQueue.length) {
-        queue = currentQueue.concat(queue);
-    } else {
-        queueIndex = -1;
-    }
-    if (queue.length) {
-        drainQueue();
-    }
-}
+	bold: [1, 22],
+	italic: [3, 23],
+	underline: [4, 24],
+	inverse: [7, 27],
+	strikethrough: [9, 29],
 
-function drainQueue() {
-    if (draining) {
-        return;
-    }
-    var timeout = setTimeout(cleanUpNextTick);
-    draining = true;
+	black: [30, 39],
+	red: [31, 39],
+	green: [32, 39],
+	yellow: [33, 39],
+	blue: [34, 39],
+	magenta: [35, 39],
+	cyan: [36, 39],
+	white: [37, 39],
+	gray: [90, 39],
 
-    var len = queue.length;
-    while(len) {
-        currentQueue = queue;
-        queue = [];
-        while (++queueIndex < len) {
-            if (currentQueue) {
-                currentQueue[queueIndex].run();
-            }
-        }
-        queueIndex = -1;
-        len = queue.length;
-    }
-    currentQueue = null;
-    draining = false;
-    clearTimeout(timeout);
-}
-
-process.nextTick = function (fun) {
-    var args = new Array(arguments.length - 1);
-    if (arguments.length > 1) {
-        for (var i = 1; i < arguments.length; i++) {
-            args[i - 1] = arguments[i];
-        }
-    }
-    queue.push(new Item(fun, args));
-    if (queue.length === 1 && !draining) {
-        setTimeout(drainQueue, 0);
-    }
+	bgBlack: [40, 49],
+	bgRed: [41, 49],
+	bgGreen: [42, 49],
+	bgYellow: [43, 49],
+	bgBlue: [44, 49],
+	bgMagenta: [45, 49],
+	bgCyan: [46, 49],
+	bgWhite: [47, 49]
 };
 
-// v8 likes predictible objects
-function Item(fun, array) {
-    this.fun = fun;
-    this.array = array;
-}
-Item.prototype.run = function () {
-    this.fun.apply(null, this.array);
-};
-process.title = 'browser';
-process.browser = true;
-process.env = {};
-process.argv = [];
-process.version = ''; // empty string to avoid regexp issues
-process.versions = {};
+Object.keys(codes).forEach(function (key) {
+	var val = codes[key];
+	var style = styles[key] = {};
+	style.open = '\x1b[' + val[0] + 'm';
+	style.close = '\x1b[' + val[1] + 'm';
+});
 
-function noop() {}
-
-process.on = noop;
-process.addListener = noop;
-process.once = noop;
-process.off = noop;
-process.removeListener = noop;
-process.removeAllListeners = noop;
-process.emit = noop;
-
-process.binding = function (name) {
-    throw new Error('process.binding is not supported');
-};
-
-process.cwd = function () { return '/' };
-process.chdir = function (dir) {
-    throw new Error('process.chdir is not supported');
-};
-process.umask = function() { return 0; };
-
-},{}],10:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 /*
 	PseudoClass - JavaScript inheritance
 
@@ -855,6 +767,99 @@ process.umask = function() { return 0; };
 		}
 	}
 }(this));
+
+},{}],10:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = setTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    clearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        setTimeout(drainQueue, 0);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
 
 },{}],11:[function(require,module,exports){
 'use strict';
