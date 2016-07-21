544ae33e1ecc9ae34213e36d22eb8c6f2c3753f4
Maxime Tricoire <max.tricoire@gmail.com>
2016-07-21 15:32:03 +0200

    v3.0.0-alpha: prepare for new registry

--- kevoree-commons.js
+++ kevoree-commons.js
@@ -1,4 +1,3 @@
 module.exports.Resolver      = require('./lib/Resolver');
 module.exports.Bootstrapper  = require('./lib/Bootstrapper');
 module.exports.KevoreeLogger = require('./lib/KevoreeLogger');
-module.exports.FileSystem    = require('./lib/FileSystem');
